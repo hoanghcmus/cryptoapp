faa712566c56467db8b03b3beb0554d5a628343c
Steve Nguyen <hoanghcmus@gmail.com>
2025-09-18 09:00:04 +0700

    fix: CA-01 jest test issue

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  moduleNameMapper: {
+    '^.+\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@tanstack/react-query)/)'
+  ],
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import '../global.css';
+// global.css includes Tailwind directives; import it at runtime if available.
+try {
+  require('../global.css');
+} catch (e) {
+  // ignore in test environment where CSS imports are not handled
+}
+
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import DemoScreen from './activitys/DemoScreen';
