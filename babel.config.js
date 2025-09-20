0b223964448c985321fefb2425a59bbae88cdcf8
Steve Nguyen <hoanghcmus@gmail.com>
2025-09-20 12:46:09 +0700

    fix: CA-01 iOS app could not run due to babel config

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      'module:metro-react-native-babel-preset',
-      '@babel/preset-typescript', 
       ["module:@react-native/babel-preset", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
